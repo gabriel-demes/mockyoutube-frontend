6ba7226e5ce0109fd761f180d7f0e0abb50eef8d
celestialheathen <jingchaoyu@hotmail.com>
2021-03-29 12:04:23 -0400

    Added number of comments in Comments, likes and dislikes btn in Commment

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,12 +1,41 @@
-import React from "react"
+import React, {useState} from "react"
 import '../css/Comment.css'
 
 const Comment = ({user, body}) => {
+    const [thumbUp, setThumbUp] = useState(false)
+    const [likes, setLikes] = useState(3)
+    const [thumbDown, setThumbDown] = useState(false)
+    const [dislikes, setDislikes] = useState(4)
+
+    function handleLike() {
+        if (thumbUp) {
+            setThumbUp(false)
+            setLikes(likes - 1)
+        } else {
+            setThumbUp(true)
+            setLikes(likes  + 1)
+        }
+    }
+
+    function handleDislike() {
+        if (thumbDown) {
+            setThumbDown(false) 
+            setDislikes(dislikes - 1)
+        } else {
+            setThumbDown(true)
+            setDislikes(dislikes + 1)
+        }
+    }
 
     return (
+
         <div className="comment">
             <h6 style={{display:"inline"}}>{user} </h6>
             <p style={{display:"inline"}}>{body}</p>
+            <div class="comment-likes">
+                <span className="thumbup" onClick={handleLike}>👍</span>&nbsp;&nbsp;<span id="likes-counter">{likes !== 0 ? likes: null }</span> &nbsp;&nbsp;&nbsp;&nbsp;
+                <span className="thumbdown" onClick={handleDislike}>👎</span>&nbsp;&nbsp;<span id="dislikes-counter">{ dislikes !== 0 ? dislikes : null }</span>
+            </div>
         </div>
     )
 }
--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useState} from "react"
 import Comment from "./Comment"
 import '../css/Comments.css'
 
@@ -10,11 +10,23 @@ const Comments = () => {
         return hardComments.map(comment => <Comment user={comment.user} body={comment.body}/>)
     }
 
+    const [body, setBody] = useState("")
+
+    function postNewComment(e) {
+        e.preventDefault()
+    }
+
     return (
-        <div className="comments">
-            <h5>Comments</h5>
-            {displayComments()}
-        </div>
+        <>
+            <form id="new-comment-form" onSubmit={postNewComment}>
+                <input type="text" placeholder="Add a comment..." id="comment-field" value={body} onChange={e => setBody(e.target.value)}></input>
+                <input type="submit" value="Comment" id="submit-btn"></input>
+            </form>
+            <div className="comments">
+                <h5>{displayComments().length} Comments</h5>
+                {displayComments()}
+            </div>
+        </>
     )
 }
 
